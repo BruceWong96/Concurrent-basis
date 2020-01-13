336afab5e5b7584479bb5bedcdfaa62cfbe104fc
FerdinandWang <603334301@qq.com>
2020-01-13 20:42:06 +0800

    Update Demo.java

--- src/concurrent/demo/queue/Demo.java
+++ src/concurrent/demo/queue/Demo.java
@@ -28,6 +28,8 @@ public class Demo {
         BlockingQueue queue = new ArrayBlockingQueue<>(10);
         for (int i=0 ; i<10 ; i++){
             queue.put(i);
+
+            
         }
 
 //        queue.add(11);
