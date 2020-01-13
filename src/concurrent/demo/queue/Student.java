2139df51d52bbd982134d51a15f11ee27c8506ac
FerdinandWang <603334301@qq.com>
2020-01-13 20:55:47 +0800

    Update Student.java

--- src/concurrent/demo/queue/Student.java
+++ src/concurrent/demo/queue/Student.java
@@ -5,10 +5,10 @@ public class Student implements Comparable<Student>{
     private String name;
     private int score;
 
+
     public Student(String name, int score) {
         this.name = name;
         this.score = score;
-        
     }
 
     @Override
